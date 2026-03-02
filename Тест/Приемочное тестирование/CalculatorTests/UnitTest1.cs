29f9fca9f9de7f655e4caf9dd4e250777d0ce3fb
hrafetresa <hrafetresa@gmail.com>
2026-03-02 03:47:27 +0300

    MS, MR, MC, M+, M-, /0, -Sqrt

--- Тест/Приемочное тестирование/CalculatorTests/UnitTest1.cs
+++ Тест/Приемочное тестирование/CalculatorTests/UnitTest1.cs
@@ -206,5 +206,209 @@ namespace CalculatorTests
                 app.Close();
             }
         }
+
+        [TestMethod]
+        public void MSMethod()
+        {
+            var app = FlaUI.Core.Application.Launch(@"C:\Users\User\Desktop\Тесты\Тест\Тест\Приемочное тестирование\CalculatorTests\bin\Debug\Calculator.exe");
+            using (var automation = new UIA3Automation())
+            {
+                //arrange data
+                var window = app.GetMainWindow(automation);
+                var twoButton = window.FindFirstDescendant(cf => cf.ByName("2"))?.AsButton();
+                var msButton = window.FindFirstDescendant(cf => cf.ByName("MS"))?.AsButton();
+                var clearButton = window.FindFirstDescendant(cf => cf.ByName("C"))?.AsButton();
+                var mrButton = window.FindFirstDescendant(cf => cf.ByName("MR"))?.AsButton();
+
+                //act
+                twoButton?.Click();
+                msButton?.Click();
+                clearButton?.Click();
+                mrButton?.Click();
+
+                var resultField = window.FindFirstDescendant(cf => cf.ByClassName("WindowsForms10.EDIT.app.0.141b42a_r7_ad1")).AsTextBox();
+
+                //assert
+                Assert.AreEqual("2", resultField?.Text);
+
+                app.Close();
+            }
+        }
+
+        [TestMethod]
+        public void MRMethod()
+        {
+            var app = FlaUI.Core.Application.Launch(@"C:\Users\User\Desktop\Тесты\Тест\Тест\Приемочное тестирование\CalculatorTests\bin\Debug\Calculator.exe");
+            using (var automation = new UIA3Automation())
+            {
+                //arrange data
+                var window = app.GetMainWindow(automation);
+                var eightButton = window.FindFirstDescendant(cf => cf.ByName("8"))?.AsButton();
+                var msButton = window.FindFirstDescendant(cf => cf.ByName("MS"))?.AsButton();
+                var mrButton = window.FindFirstDescendant(cf => cf.ByName("MR"))?.AsButton();
+                var clearButton = window.FindFirstDescendant(cf => cf.ByName("C"))?.AsButton();
+
+                //act
+                eightButton?.Click();
+                msButton?.Click();
+                clearButton.Click();
+                mrButton?.Click();
+
+                var resultField = window.FindFirstDescendant(cf => cf.ByClassName("WindowsForms10.EDIT.app.0.141b42a_r7_ad1")).AsTextBox();
+
+                //assert
+                Assert.AreEqual("8", resultField?.Text);
+
+                app.Close();
+            }
+        }
+
+        [TestMethod]
+        public void MCMethod()
+        {
+            var app = FlaUI.Core.Application.Launch(@"C:\Users\User\Desktop\Тесты\Тест\Тест\Приемочное тестирование\CalculatorTests\bin\Debug\Calculator.exe");
+            using (var automation = new UIA3Automation())
+            {
+                //arrange data
+                var window = app.GetMainWindow(automation);
+                var nineButton = window.FindFirstDescendant(cf => cf.ByName("9"))?.AsButton();
+                var msButton = window.FindFirstDescendant(cf => cf.ByName("MS"))?.AsButton();
+                var mcButton = window.FindFirstDescendant(cf => cf.ByName("MC"))?.AsButton();
+                var mrButton = window.FindFirstDescendant(cf => cf.ByName("MR"))?.AsButton();
+
+                //act
+                nineButton?.Click();
+                msButton?.Click();
+                mcButton?.Click();
+                mrButton?.Click();
+
+                var resultField = window.FindFirstDescendant(cf => cf.ByClassName("WindowsForms10.EDIT.app.0.141b42a_r7_ad1")).AsTextBox();
+
+                //assert
+                Assert.AreEqual("0", resultField?.Text);
+
+                app.Close();
+            }
+        }
+
+        [TestMethod]
+        public void MPlusMethod()
+        {
+            var app = FlaUI.Core.Application.Launch(@"C:\Users\User\Desktop\Тесты\Тест\Тест\Приемочное тестирование\CalculatorTests\bin\Debug\Calculator.exe");
+            using (var automation = new UIA3Automation())
+            {
+                //arrange data
+                var window = app.GetMainWindow(automation);
+                var twoButton = window.FindFirstDescendant(cf => cf.ByName("2"))?.AsButton();
+                var threeButton = window.FindFirstDescendant(cf => cf.ByName("3"))?.AsButton();
+                var msButton = window.FindFirstDescendant(cf => cf.ByName("MS"))?.AsButton();
+                var mPlusButton = window.FindFirstDescendant(cf => cf.ByName("M+"))?.AsButton();
+                var mrButton = window.FindFirstDescendant(cf => cf.ByName("MR"))?.AsButton();
+                var clearButton = window.FindFirstDescendant(cf => cf.ByName("C"))?.AsButton();
+
+                //act
+                twoButton?.Click();
+                msButton?.Click();
+                clearButton.Click();
+                threeButton?.Click();
+                mPlusButton?.Click();
+                mrButton?.Click();
+
+                var resultField = window.FindFirstDescendant(cf => cf.ByClassName("WindowsForms10.EDIT.app.0.141b42a_r7_ad1")).AsTextBox();
+
+                //assert
+                Assert.AreEqual("5", resultField?.Text);
+
+                app.Close();
+            }
+        }
+
+        [TestMethod]
+        public void MMinusMethod()
+        {
+            var app = FlaUI.Core.Application.Launch(@"C:\Users\User\Desktop\Тесты\Тест\Тест\Приемочное тестирование\CalculatorTests\bin\Debug\Calculator.exe");
+            using (var automation = new UIA3Automation())
+            {
+                //arrange data
+                var window = app.GetMainWindow(automation);
+                var nineButton = window.FindFirstDescendant(cf => cf.ByName("9"))?.AsButton();
+                var oneButton = window.FindFirstDescendant(cf => cf.ByName("1"))?.AsButton();
+                var msButton = window.FindFirstDescendant(cf => cf.ByName("MS"))?.AsButton();
+                var mMinusButton = window.FindFirstDescendant(cf => cf.ByName("M-"))?.AsButton();
+                var mrButton = window.FindFirstDescendant(cf => cf.ByName("MR"))?.AsButton();
+                var clearButton = window.FindFirstDescendant(cf => cf.ByName("C"))?.AsButton();
+
+                //act
+                nineButton?.Click();
+                msButton?.Click();
+                clearButton.Click();
+                oneButton?.Click();
+                mMinusButton?.Click();
+                mrButton?.Click();
+
+                var resultField = window.FindFirstDescendant(cf => cf.ByClassName("WindowsForms10.EDIT.app.0.141b42a_r7_ad1")).AsTextBox();
+
+                //assert
+                Assert.AreEqual("8", resultField?.Text);
+
+                app.Close();
+            }
+        }
+
+        [TestMethod]
+        public void DivideByZeroMethod()
+        {
+            var app = FlaUI.Core.Application.Launch(@"C:\Users\User\Desktop\Тесты\Тест\Тест\Приемочное тестирование\CalculatorTests\bin\Debug\Calculator.exe");
+            using (var automation = new UIA3Automation())
+            {
+                //arrange data
+                var window = app.GetMainWindow(automation);
+                var fiveButton = window.FindFirstDescendant(cf => cf.ByName("5"))?.AsButton();
+                var zeroButton = window.FindFirstDescendant(cf => cf.ByName("0"))?.AsButton();
+                var divideButton = window.FindFirstDescendant(cf => cf.ByName("/"))?.AsButton();
+                var resultButton = window.FindFirstDescendant(cf => cf.ByName("="))?.AsButton();
+
+                //act
+                fiveButton?.Click();
+                divideButton?.Click();
+                zeroButton?.Click();
+                resultButton?.Click();
+
+                //assert
+                var okButton = window.FindFirstDescendant(cf => cf.ByName("ОК")).AsButton();
+                Assert.IsNotNull(okButton, "Окно с ошибкой не появилось!");
+                okButton?.Click();
+                app.Close();
+            }
+        }
+
+        [TestMethod]
+        public void NegativeSqrtMethod()
+        {
+            var app = FlaUI.Core.Application.Launch(@"C:\Users\User\Desktop\Тесты\Тест\Тест\Приемочное тестирование\CalculatorTests\bin\Debug\Calculator.exe");
+            using (var automation = new UIA3Automation())
+            {
+                //arrange data
+                var window = app.GetMainWindow(automation);
+                var threeButton = window.FindFirstDescendant(cf => cf.ByName("3"))?.AsButton();
+                var btnMinus = window.FindFirstDescendant(cf => cf.ByName("-"))?.AsButton();
+                var sixButton = window.FindFirstDescendant(cf => cf.ByName("6"))?.AsButton();
+                var resultButton = window.FindFirstDescendant(cf => cf.ByName("="))?.AsButton();
+                var sqrtButton = window.FindFirstDescendant(cf => cf.ByName("√"))?.AsButton();
+
+                //act
+                threeButton?.Click();
+                btnMinus?.Click();
+                sixButton?.Click();
+                resultButton?.Click();
+                sqrtButton?.Click();
+
+                //assert
+                var okButton = window.FindFirstDescendant(cf => cf.ByName("ОК")).AsButton();
+                Assert.IsNotNull(okButton, "Окно с ошибкой не появилось!");
+                okButton?.Click();
+                app.Close();
+            }
+        }
     }
 }
